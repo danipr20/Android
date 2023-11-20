dc7d24685225c9e663024ec80fdcb6e85d960183
Daniel <danipr2000@gmail.com>
2023-11-20 23:15:05 +0100

    subida del ej 1

--- Practica1/app/src/main/java/infoii/dpr20/practica1/MainActivity.java
+++ Practica1/app/src/main/java/infoii/dpr20/practica1/MainActivity.java
@@ -3,15 +3,46 @@ package infoii.dpr20.practica1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
 
+    private Button boton1;
+    private Button boton2;
 
+    private TextView texto;
+    private int contador=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        boton1 = (Button) findViewById(R.id.boton);
+        boton2 = (Button) findViewById(R.id.pulsador);
+        texto = (TextView) findViewById(R.id.text_cont);
+        boton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            contador++;
+            texto.setText("CONTADOR= "+contador);
+            }
+
+
+        });
+        boton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                contador--;
+                texto.setText("CONTADOR= "+contador);
+            }
+
+
+        });
     }
+
 }
