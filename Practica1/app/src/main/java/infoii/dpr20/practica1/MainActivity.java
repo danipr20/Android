438ce80a5abbd5b75105845fd5b5f706e8d28ca1
Daniel <danipr2000@gmail.com>
2023-11-21 10:37:27 +0100

    comienzo de calculadora, botones numericos funciona

--- Practica1/app/src/main/java/infoii/dpr20/practica1/MainActivity.java
+++ Practica1/app/src/main/java/infoii/dpr20/practica1/MainActivity.java
@@ -10,22 +10,51 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
 
-    private Button boton1;
-    private Button boton2;
+    private Button puls1;
+    private Button puls2;
 
     private TextView texto;
+    private TextView Resultado;
     private int contador=0;
+    private String valorA="";
+    private String valorB="";
+    private String res="";
 
-    @Override
+    private Button boton1; Button boton2;Button boton3;Button boton4;Button boton5;Button boton6;Button boton7;Button boton8;Button boton9;Button boton0;
+    private Button botonAC;Button botonC;Button botonSuma;Button botonResta;Button botonMulti;Button botonDivi;Button botonIgual;Button botonComa;
+
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        boton1 = (Button) findViewById(R.id.boton);
-        boton2 = (Button) findViewById(R.id.pulsador);
+        puls1 = (Button) findViewById(R.id.boton);
+        puls2 = (Button) findViewById(R.id.pulsador);
         texto = (TextView) findViewById(R.id.text_cont);
-        boton1.setOnClickListener(new View.OnClickListener() {
+
+        //Calculadora
+
+        boton1 = (Button) findViewById(R.id.boton1);
+        boton2 = (Button) findViewById(R.id.boton2);
+        boton3 = (Button) findViewById(R.id.boton3);
+        boton4 = (Button) findViewById(R.id.boton4);
+        boton5 = (Button) findViewById(R.id.boton5);
+        boton6 = (Button) findViewById(R.id.boton6);
+        boton7 = (Button) findViewById(R.id.boton7);
+        boton8 = (Button) findViewById(R.id.boton8);
+        boton9 = (Button) findViewById(R.id.boton9);
+        boton0 = (Button) findViewById(R.id.boton0);
+        botonSuma = (Button) findViewById(R.id.suma);
+        botonResta = (Button) findViewById(R.id.resta);
+        botonDivi = (Button) findViewById(R.id.division);
+        botonMulti= (Button) findViewById(R.id.multiplicacion);
+        botonIgual = (Button) findViewById(R.id.igual);
+        botonAC = (Button) findViewById(R.id.boton16);
+        botonC = (Button) findViewById(R.id.boton12);
+        Resultado = (TextView) findViewById(R.id.result);
+
+
+        puls1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
             contador++;
@@ -34,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         });
-        boton2.setOnClickListener(new View.OnClickListener() {
+        puls2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 contador--;
@@ -43,6 +72,133 @@ public class MainActivity extends AppCompatActivity {
 
 
         });
+        boton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               valorA=valorA.concat("1");
+                Resultado.setText(valorA);
+            }
+        });
+
+        boton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valorA=valorA.concat("2");
+                Resultado.setText(valorA);
+            }
+        });
+
+        boton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valorA=valorA.concat("3");
+                Resultado.setText(valorA);
+            }
+        });
+
+        boton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valorA=valorA.concat("4");
+                Resultado.setText(valorA);
+            }
+        });
+
+        boton5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valorA=valorA.concat("5");
+                Resultado.setText(valorA);
+            }
+        });
+
+        boton6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valorA=valorA.concat("6");
+                Resultado.setText(valorA);
+            }
+        });
+
+        boton7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valorA=valorA.concat("7");
+                Resultado.setText(valorA);
+            }
+        });
+
+        boton8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valorA=valorA.concat("8");
+                Resultado.setText(valorA);
+            }
+        });
+
+        boton9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valorA=valorA.concat("9");
+                Resultado.setText(valorA);
+            }
+        });
+
+        boton0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valorA=valorA.concat("0");
+                Resultado.setText(valorA);
+            }
+        });
+
+        botonSuma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        botonResta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        botonMulti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        botonDivi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
 }
